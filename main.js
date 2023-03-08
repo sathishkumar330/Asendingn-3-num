fc8b4c067085cb4b3531c69162953f009022252c
sathishkumar330 <svksathish41@gmail.com>
2023-03-08 10:45:01 -0500

    add

---
+++ main.js
@@ -0,0 +1,38 @@
+var a=25;
+var b=22;
+var c=20;
+
+if(a<b){
+	if(a<c){
+		if(c<b){
+			console.log(a,c,b);
+		}
+	
+
+else{
+	console.log(a,b,c);
+}
+}
+}
+ if(b<a){
+		if(b<c){
+			if(c<a){
+				console.log(b,c,a);
+		}
+	
+
+else{
+	console.log(b,a,c);
+}
+}
+}
+ if(c<b){
+		if(c<a){
+			if(a<b){
+				console.log(c,a,b);
+		}
+else{
+	console.log(c,b,a);
+}
+}
+}
